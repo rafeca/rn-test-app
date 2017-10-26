d2c8056892b237cd76b7295afc3ab004573359bc
Rafael Oleza <rafeca@fb.com>
2017-10-26 20:51:19 +0100

    Add some testing information

--- App.js
+++ App.js
@@ -6,11 +6,18 @@
 
 import React, { Component } from 'react';
 import {
+  Image,
   Platform,
+  ScrollView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+import config from './src/config.json';
+import yml from './src/asset.yml';
+import img from './src/img.png';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -21,17 +28,25 @@ const instructions = Platform.select({
 
 export default class App extends Component<{}> {
   render() {
+    const err = new Error('error with a stack trace');
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <ScrollView style={styles.scroll}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!!!! {config.foo}
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit App.js {yml.integer}
+          </Text>
+          <Text style={styles.instructions}>
+            {err.stack}
+          </Text>
+          <Text style={styles.instructions}>
+            {instructions}
+          </Text>
+          <Image source={img} style={styles.image} />
+        </ScrollView>
       </View>
     );
   }
@@ -44,6 +59,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  scroll: {
+    flex: 1,
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -54,4 +72,8 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  image: {
+    width: 200,
+    height: 200,
+  }
 });
