711d726e0d730894ccf52974bf8340f9e17c2fd5
Rafael Oleza <rafeca@fb.com>
2017-10-30 13:22:43 +0100

    Fix sourcemap script

--- scripts/sourcemap.js
+++ scripts/sourcemap.js
@@ -3,11 +3,11 @@
 const sourceMap = require('source-map');
 const fs = require('fs');
 
-fs.readFile(argv[1], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   const smc = new sourceMap.SourceMapConsumer(data);
 
   console.log(smc.originalPositionFor({
-    line: argv[2],
-    column: argv[3],
+    line: Number(process.argv[3]),
+    column: Number(process.argv[4]),
   }));
 });
